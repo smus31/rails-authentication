3540c818a0d5b6d4b6f74c810b4492434f7fe436
Sam Musler <sam.a.musler@gmail.com>
2023-04-24 12:50:42 -0400

    added film work and portfolio sites

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def portfolio
+  end
+
+  def movie_finder
+  end
+
+  def old_film_work
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/about', to: 'pages#about'
+  get '/portfolio', to: 'pages#portfolio'
+  get '/movie_finder', to: 'pages#movie_finder'
+  get '/old_film_work', to: 'pages#old_film_work'
+  get '/contact', to: 'pages#contact'
 end
